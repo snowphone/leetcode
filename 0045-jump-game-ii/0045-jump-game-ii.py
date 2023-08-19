379e86de63ea979704b66df76a4dc81648380a06
Junoh Moon <mjo970625@gmail.com>
2023-08-19 20:54:49 +0900

    Time: 2355 ms (28.4%), Space: 45.5 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,21 +1,21 @@
-from functools import cache
-
 class Solution:
-    def jump(self, nums: List[int], idx: int = 0) -> int:
+    def jump(self, nums: List[int]) -> int:
         n = len(nums)
 
         @cache
-        def _jmp(i: int) -> int:
-            if i == n - 1:
+        def fn(i):
+            if i >= n - 1:
                 return 0
-    
-            begin = i + 1
-            end =  min(i + nums[i] + 1, n)
-            if begin >= end:
-                return n * n
-    
-            dst = range(end - 1, begin - 1, -1)
-            return min(_jmp(it) for it in dst) + 1
+            
+            if i + nums[i] >= n - 1:
+                return 1
 
-        return _jmp(0)
+            return 1 + min(
+                (
+                    fn(j) 
+                    for j in range(i+1, i+nums[i] + 1)
+                ), default=987654321
+             )
+
+        return fn(0)
 
