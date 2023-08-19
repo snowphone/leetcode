ac50a4cf2fa434df6a566e6ac0150f3b078e0ab3
Junoh Moon <mjo970625@gmail.com>
2023-08-20 00:11:59 +0900

    Time: 96 ms (89.75%), Space: 20.9 MB (87.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,31 @@
 from collections import Counter
+from heapq import heappush, heappop
+
+class Heap(list):
+    def __init__(self, key):
+        super().__init__(self)
+        self.key = key
+    
+    def put(self, item):
+        heappush(self, (self.key(item), item) )
+    
+    def get(self):
+        return heappop(self)[1]
+
+    def peak(self):
+        return self[0][1]
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        cnt = Counter(nums)
+        counter = Counter(nums)
 
-        freqs = sorted(cnt.items(), key=itemgetter(1))
-
-        return [n for n, c in freqs[-k:]]
+        heap = Heap(key=lambda it: counter[n] )  # Pop the fewest first
+        for n in counter.keys():
+            heap.put(n)
+            while len(heap) > k:
+                heap.get()
+        
+        answer = []
+        while heap:
+            answer.append(heap.get())
+        return answer
