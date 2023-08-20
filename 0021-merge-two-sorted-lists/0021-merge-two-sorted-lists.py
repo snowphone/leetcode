b223653996225e425475ebaeaffe8d8ebbaa3015
Junoh Moon <mjo970625@gmail.com>
2023-08-20 22:32:19 +0900

    Time: 46 ms (68.3%), Space: 16.3 MB (50.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,6 +1,3 @@
-from operator import attrgetter
-from queue import PriorityQueue
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -8,40 +5,23 @@ from queue import PriorityQueue
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        lists = [list1, list2]
-        self.patch()
+        head = ListNode()  # dummy node
+        tail = head
 
-        head = None
-        tail = None
+        while list1 or list2:
+            if not list1:
+                tail.next = list2
+                break
+            if not list2:
+                tail.next = list1
+                break
+            if list1.val < list2.val:
+                nd = list1
+                list1 = list1.next
+            else:
+                nd = list2
+                list2 = list2.next
+            tail.next = nd
+            tail = tail.next
+        return head.next
 
-        pq = PriorityQueue()
-        self.pq = pq
-
-        for it in filter(None, lists):
-            pq.put( it )
-
-        while pq.qsize():
-            it = self.pq.get()
-    
-            nxt = it.next
-            if nxt:
-                self.pq.put(nxt)
-            it.next = None
-
-            if not head:
-                head = it
-                tail = it
-                continue
-
-            tail.next = it
-            tail = it
-        return head
-    
-    @staticmethod
-    def patch():
-        def lt(self, other):
-            return self.val < other.val
-        ListNode.__lt__ = lt
-
-        return
-    
