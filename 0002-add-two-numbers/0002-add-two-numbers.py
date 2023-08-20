19c2f4deadff13e6e57b50aab6f198d76ecbde16
Junoh Moon <mjo970625@gmail.com>
2023-08-20 21:50:02 +0900

    Time: 69 ms (70.2%), Space: 16.4 MB (27.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,23 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def read(self, head):
-        nd = head
-        answer = []
-        while nd:
-            answer.append(str(nd.val))
-            nd = nd.next
-        return int("".join(reversed(answer)))
-
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # l1 += l2
+        answer = l1
 
-        nd, tail = None, None
-        for n in reversed(str( self.read(l1) + self.read(l2) )) :
-            if nd is None:
-                nd = ListNode(n)
-                tail = nd
-            else:
-                tail.next = ListNode(n)
-                tail = tail.next
-        return nd
+        l1_prev, l2_prev = None, None
+        carry = 0
+        while l1 or l2 or carry:
+            if not l1:
+                l1 = ListNode()
+                l1_prev.next = l1
+            if not l2:
+                l2 = ListNode()
+                l2_prev.next = l2
+            
+            l1.val += l2.val + carry
+            carry = l1.val // 10
+            l1.val %= 10
+
+            l1_prev, l1 = l1,  l1.next
+            l2_prev, l2 = l2,  l2.next
+        
+        return answer
 
