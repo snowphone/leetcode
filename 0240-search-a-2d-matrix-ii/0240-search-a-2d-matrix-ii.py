8d3e4a3d5003f1a458babaac4c02976219d81f59
Junoh Moon <mjo970625@gmail.com>
2023-08-22 21:57:09 +0900

    Time: 158 ms (70.87%), Space: 22.8 MB (23.43%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,16 +1,21 @@
 from bisect import bisect_left
 
 class Solution:
-    def bsearch(self, line, target):
-        idx = bisect_left(line, target)
-        return idx < len(line) and line[idx] == target
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        'Time Complexity: O(m lg(n) )'
+        'Time Complexity: O( m + n )'
         n_row = len(matrix)  # m
         n_col = len(matrix[0])  # n
 
-        return any(
-            self.bsearch(line, target) for line in matrix
-        )
+        r = 0
+        c = n_col - 1
+        while r < n_row and 0 <= c:
+            it = matrix[r][c]
+            print(it)
+            if it == target:
+                return True
+            if it > target:
+                c -= 1
+            else:
+                r += 1
+        return False
 
