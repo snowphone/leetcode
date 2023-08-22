b69f991c3c04ac1fb9e1a162669df474b7d9074d
Junoh Moon <mjo970625@gmail.com>
2023-08-23 08:37:35 +0900

    Time: 1477 ms (42.97%), Space: 34.3 MB (12.74%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,18 +1,42 @@
-from sortedcontainers import SortedList
+from collections import deque
+
+class MQ(deque):
+    'Monotonic Queue'
+
+    def __init__(self, reverse=False):
+        super().__init__()
+        if reverse:
+            self._key = lambda it: self[-1] < it
+        else:
+            self._key = lambda it: self[-1] > it
+        return
+    
+    def add(self, item):
+        while self and self._key(item):
+            self.pop()
+        self.append(item)
+    
+    def remove(self, item):
+        if self[0] != item:
+            return
+        return self.popleft()
+
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        'Time Complexity: O(n lg(k) )'
+        'Time Complexity: O(n)'
         answer = []
 
-        window = SortedList()
+        window = MQ(reverse=True)
         left = 0
         for right, it in enumerate(nums):
             window.add(it)
             while right - left + 1 > k:
-                window.remove(nums[left])
+                jt = nums[left]
+                window.remove(jt)
                 left += 1
             if right - left + 1 < k:
                 continue
-            answer.append(window[-1])
+            answer.append(window[0])
+            
         return answer
