d1661e908e04a686a00218a35b9beb82ac55c4da
Junoh Moon <mjo970625@gmail.com>
2023-08-23 08:22:30 +0900

    Time: 2730 ms (5%), Space: 33.3 MB (23.33%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,18 +1,17 @@
 from sortedcontainers import SortedList
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         answer = []
-        left = 0
+
         window = SortedList()
-        for right, n in enumerate(nums):
-            window.add(n)
-
-            if len(window) < k:
-                continue
-            while len(window) > k:
-                n = nums[left]
-                window.remove(n)
+        left = 0
+        for right, it in enumerate(nums):
+            window.add(it)
+            while right - left + 1 > k:
+                window.remove(nums[left])
                 left += 1
-
+            if right - left + 1 < k:
+                continue
             answer.append(window[-1])
         return answer
