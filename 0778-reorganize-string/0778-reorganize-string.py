bdc1552d86b14c58e3674b4d9e74c94ddb224e4b
Junoh Moon <mjo970625@gmail.com>
2023-08-23 20:23:05 +0900

    Time: 36 ms (91.63%), Space: 16.4 MB (9.58%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -18,6 +18,7 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         counter = Counter(s)
         q = PQ(key=lambda tpl: -tpl[1])
+
         for it in counter.items():
             q.put(it)
         answer = []
@@ -28,26 +29,26 @@ class Solution:
                 answer += [ch1, ch]
             else:
                 answer += [ch, ch1]
-            #print(answer)
 
-            cnt -= 1
-            cnt1 -= 1
+            cnt -= 1; cnt1 -= 1
             if cnt:
                 q.put( (ch, cnt) )
             if cnt1:
                 q.put( (ch1, cnt1) )
+
+        ans_str = ''.join(answer)
+        if not q:
+            return ans_str
                 
-        if q:
-            ch, cnt = q.get()
-            if cnt > 1:
-                return ''
-            if not answer:
-                answer.append(ch)
-            elif ch != answer[-1]:
-                answer.append(ch)
-            elif ch != answer[0]:
-                answer.insert(0, ch)
-            else:
-                return ''
-        return ''.join(answer)
+        ch, cnt = q.get()
+        if cnt > 1:
+            return ''
+        if not answer:
+            return ch
+        elif ch != answer[-1]:
+            return ans_str + ch
+        elif ch != answer[0]:
+            return ch + ans_str
+        else:
+            return ''
 
