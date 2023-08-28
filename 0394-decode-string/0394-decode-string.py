ff93f472233fb5f7ea79c4ebcc69f0bfa300ac91
Junoh Moon <mjo970625@gmail.com>
2023-08-28 23:12:22 +0900

    Time: 36 ms (81.82%), Space: 16.4 MB (38.08%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,64 @@
+from re import compile
+from collections import deque
+
+
+# expr := num [ expr ] | str
+# num := \d+
+# str := [a-z]+
+
+
+class INode:
+    def __init__(self, tokens: deque[str]):
+        self.tokens = tokens
+
+    def execute(self):
+        ...
+
+
+class Str(INode):
+    def execute(self):
+        return self.tokens.popleft()
+
+
+class Number(INode):
+    def execute(self):
+        return int(self.tokens.popleft())
+
+
+def is_number(token: str):
+    try:
+        int(token)
+        return True
+    except:
+        return False
+
+
+class Expr(INode):
+    def execute(self):
+        stk = []
+        while self.tokens:
+            token = self.tokens[0]
+            if is_number(token):
+                node = Number(self.tokens)
+                stk.append(node.execute())
+            elif token == "[":
+                self.tokens.popleft()
+                node = Expr(self.tokens)
+                stk[-1] = stk[-1] * node.execute()
+            elif token == "]":
+                self.tokens.popleft()
+                break
+            else:  # str
+                stk.append(Str(self.tokens).execute())
+        return "".join(stk)
+
+
+class Solution:
+    token_pattern = compile(r"\d+|\[|\]|[a-z]+")
+
+    def decodeString(self, s: str) -> str:
+        tokens = deque(it.group() for it in self.token_pattern.finditer(s))
+        exprs = []
+        while tokens:
+            exprs.append(Expr(tokens).execute())
+        return "".join(exprs)
