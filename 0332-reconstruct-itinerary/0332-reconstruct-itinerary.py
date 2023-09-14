69826df7c0081a2860e6891f53ccc835746ab6be
Junoh Moon <mjo970625@gmail.com>
2023-09-14 09:57:58 +0900

    Time: 116 ms (10.37%), Space: 17.2 MB (35.85%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,25 @@
+from queue import SimpleQueue
+from sortedcontainers import SortedList
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(SortedList)
+
+        for f, t in tickets:
+            graph[f].add(t)
+
+        def dfs(frm, graph, n_node):
+            if not n_node:
+                return True, [frm]
+
+            for it in sorted(graph[frm]):
+                graph[frm].remove(it)
+                ok, ans = dfs(it, graph, n_node - 1)
+                if ok:
+                    graph[frm].add(it)
+                    return True, [frm, *ans]
+                graph[frm].add(it)
+            return False, []
+        
+        ok, answer = dfs("JFK", graph, sum(len(it) for it in graph.values()) )
+        return answer
