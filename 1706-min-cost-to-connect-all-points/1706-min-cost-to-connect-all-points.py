d716cc5fc48e7a8a014f58a2f991476400e4765c
Junoh Moon <mjo970625@gmail.com>
2023-09-15 15:54:17 +0900

    Time: 558 ms (98.71%), Space: 16.8 MB (98.13%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -1,36 +1,29 @@
 from queue import PriorityQueue
 from heapq import heappush, heappop
+from operator import itemgetter
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         def manhattan(lhs, rhs):
             return abs(lhs[0] - rhs[0]) + abs(lhs[1] - rhs[1])
 
-        points = [tuple(it) for it in points]
+        # remaining, cost
+        remaining = { tuple(it): float('inf') for it in points }
         
-        it = points[0]
-        used = { it }
-        q = PriorityQueue()
-        for jt in points:
-            if it == jt:
-                continue
-            q.put( (manhattan(it, jt), jt) )
+        it = next(iter(remaining))
+        remaining.pop(it)
+
+        for k, v in remaining.items():
+            remaining[k] = min(v, manhattan(it, k))
 
         answer = 0
-        n = len(points)
-        while len(used) < n:
-            cost, it = q.get()
-            if it in used:
-                continue
-
+        while remaining:
+            it, cost = min(remaining.items(), key=itemgetter(1))
             answer += cost
-            used.add(it)
 
-
-            for jt in points:
-                if it == jt:
-                    continue
-                q.put( (manhattan(it, jt), jt) )
+            remaining.pop(it)
+            for k, v in remaining.items():
+                remaining[k] = min(v, manhattan(it, k))
 
         return answer
         
