4598b8526538b206f6468a16f45570e1b077247b
Junoh Moon <mjo970625@gmail.com>
2023-10-05 22:50:53 +0900

    Time: 268 ms (26.49%), Space: 20.5 MB (15.22%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,8 +2,12 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         cnt = 0
         acc = 0
+
+        # key: sum(nums[:i+1])
+        # value: if not zero, it stands for s(i) exists such that s(j) - s(i) = k <=> sum(nums[i+1:j+1]) = k
+        #                     and there's `value` i's exist.
         m = defaultdict(int)
-        m[0] = 1
+        m[0] = 1  # To count myself
 
         for it in nums:
             acc += it
