1d3196fa0df20c9840b60a370f35bb3f27f8c465
Junoh Moon <mjo970625@gmail.com>
2023-10-05 22:12:53 +0900

    Time: 278 ms (10.58%), Space: 20.5 MB (17.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,19 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        @cache
-        def acc(i: int):
-            'Accumulate 0..i, inclusive'
-            if i == 0:
-                return nums[0]
-            return acc(i-1) + nums[i]
+        cnt = 0
+        acc = 0
+        m = defaultdict(int)
+        m[0] = 1
 
-        answer = 0
-        n = len(nums)
-        cnt = defaultdict(int)  # sum: cnt
-        cnt[0] = 1
-        for j in range(n):
-            if acc(j) - k in cnt:  # i such that sum(j) - sum(i) == k exists!
-                answer += cnt[acc(j) - k]
-            cnt[acc(j)] += 1
+        for it in nums:
+            acc += it
+            cnt += m[acc - k]
+
+            m[acc] += 1
+        return cnt
         
-        return answer
