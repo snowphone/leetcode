0867b88493685f9c353242098ddd843b88e88ef8
Junoh Moon <mjo970625@gmail.com>
2023-10-17 12:11:48 +0900

    Time: 313 ms (20.36%), Space: 37.3 MB (5.09%) - LeetHub

---
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        indegree = [0] * n
+        graph = defaultdict(list)
+
+        for i in range(n):
+            if leftChild[i] != -1:
+                graph[i].append(leftChild[i])
+                indegree[leftChild[i]] += 1
+            if rightChild[i] != -1:
+                graph[i].append(rightChild[i])
+                indegree[rightChild[i]] += 1
+        
+        roots = [ i for i, it in enumerate(indegree) if it == 0 ]
+        if len(roots) != 1:
+            return False
+        root = roots[0]
+        
+        visited = set()
+
+        def dfs(root):
+            if root in visited:
+                return False
+            
+            visited.add(root)
+            
+            return all(
+                dfs(child) for child in graph[root]
+            )
+        
+        return dfs(root) and len(visited) == n
