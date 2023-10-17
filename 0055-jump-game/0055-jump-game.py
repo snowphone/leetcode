fb16dd6beb63279e5fdee3e2d4f4fc7c95f5b26b
Junoh Moon <mjo970625@gmail.com>
2023-10-17 23:25:11 +0900

    Time: 4028 ms (11.31%), Space: 46.7 MB (5.18%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,20 +1,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n  = len(nums)
 
         @cache
-        def fn(i: int):
-            if i >= n - 1:
+        def fn(idx: int):
+            if idx == len(nums) - 1:
                 return True
-            
-            if i + nums[i] >= n - 1:
-                return True
-            
+            if idx >= len(nums):
+                return False
 
-            return any(
-                fn(j) for j in range(i+1, i + nums[i] + 1)
-            )
-        for i in range(n-1, 0, -1):
-            fn(i)  # Warm up cache
-            
+            n = nums[idx]
+            return any(fn(i) for i in range(idx + n, idx, -1) )
+
         return fn(0)
