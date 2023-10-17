74900c618bd63dda1fe104328e65f1a788b36259
Junoh Moon <mjo970625@gmail.com>
2023-10-17 22:37:09 +0900

    Time: 168 ms (5.01%), Space: 24.6 MB (5.27%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def valid(s: str):
+            stk = []
+            for ch in s:
+                if stk and (stk[-1], ch) == ('(', ')'):
+                    stk.pop()
+                else:
+                    stk.append(ch)
+            return not stk
+
+        def fn(nchars: int):
+            if nchars == 0:
+                return ['']
+            sub = fn(nchars - 1)
+
+            answer = []
+            for tmp in sub:
+                answer += [
+                    tmp + '(',
+                    tmp + ')',
+                ]
+            return answer
+        
+        return [
+            it for it in  fn(n * 2) if valid(it)
+        ]
