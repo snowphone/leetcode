022628ab659b970b54576c801569bae5c0b935c8
Junoh Moon <mjo970625@gmail.com>
2023-10-17 22:16:13 +0900

    Time: 43 ms (34.89%), Space: 17.1 MB (5.53%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -3,18 +3,13 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         if not s:
             return 1
-        if len(s) == 1:
-            if s == '0':
-                return 0
-            return 1
         
         ch = s[0]
         if ch == '0':
             return 0
         if '3' <= ch <= '9':
             return self.numDecodings(s[1:])
-        ch = s[:2]
-        if '10' <= ch <= '26':
+        if 10 <= int(s[:2]) <= 26:
             return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
         return self.numDecodings(s[1:])
 
