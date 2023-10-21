b7c78dc3b37a6e2e69a44b1a560a1fec624eabda
Junoh Moon <mjo970625@gmail.com>
2023-10-21 13:51:46 +0900

    Time: 0 ms (100%), Space: 40.5 MB (69.25%) - LeetHub

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/2015-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for (int i = 0; i < 4; ++i) {
+            if( equals(mat, target) ) {
+                return true;
+            }
+            rotate(mat);
+        }
+        return false;
+    }
+
+    private boolean equals(int[][] lhs, int[][] rhs) {
+        var n_row = lhs.length;
+        var n_col = rhs.length;
+
+        for (int r = 0; r < n_row; r++) {
+            for (int c = 0; c < n_col; c++) {
+                if (lhs[r][c] != rhs[r][c]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void rotate(int[][] matrix) {
+        var n_row = matrix.length;
+        var n_col = matrix[0].length;
+
+        transpose(matrix);
+
+        for (int r = 0; r < n_row; r++) {
+            reverse(matrix[r]);
+        }
+    }
+
+    private void reverse(int[] list) {
+        var n = list.length;
+        var l = 0;
+        var r = n - 1;
+        while (l < r) {
+            var tmp = list[l];
+            list[l] = list[r];
+            list[r] = tmp;
+
+            l++;
+            r--;
+        }
+    }
+
+    private void transpose(int[][] matrix) {
+        var n_row = matrix.length;
+        var n_col = matrix[0].length;
+
+        for (int r = 0; r < n_row; r++) {
+            for (int c = r + 1; c < n_col; c++) {
+                var tmp = matrix[r][c];
+                matrix[r][c] = matrix[c][r];
+                matrix[c][r] = tmp;
+            }
+        }
+    }
+}
