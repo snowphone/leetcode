233bc342f84831017e3eb6088e5ba3fcd4f1932d
Junoh Moon <mjo970625@gmail.com>
2023-10-21 20:57:31 +0900

    Time: 12 ms (8.61%), Space: 44.2 MB (73.82%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(it -> it[0]));
+        
+
+        Queue<int[]> rooms = new PriorityQueue<>(Comparator.comparingInt(it -> it[1]));
+
+        rooms.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; ++i) {
+            var it = intervals[i];
+            if (rooms.peek()[1] <= it[0]) {
+                rooms.remove();
+            }
+            rooms.add(it);
+        }
+        return rooms.size();
+    }
+}
