e0f1bbedd80085bb0ee2709794a3be68168cb6b3
Junoh Moon <mjo970625@gmail.com>
2023-10-21 14:20:17 +0900

    Time: 610 ms (11.45%), Space: 32.6 MB (79.55%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        if n == 1:
+            return [[s]]
+
+        answer = []
+        if s == s[::-1]:
+            answer.append([s])
+            
+        for i in range(1, n):
+            head, tail = s[:i], s[i:]
+            if head != head[::-1]:
+                continue
+            subanswer = self.partition(tail)
+            answer += [
+                [head, *sub] for sub in subanswer
+            ]
+        return answer
