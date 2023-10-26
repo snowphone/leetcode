b8090e85504bd10b1791a719bb3411aa32b7eac9
Junoh Moon <mjo970625@gmail.com>
2023-10-26 23:33:30 +0900

    Time: 310 ms (67.5%), Space: 17.2 MB (32.08%) - LeetHub

---
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left
+from functools import cache
+
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        arr.sort()
+
+        @cache
+        def fn(idx: int):
+            if idx == 0:
+                return 1
+            n = arr[idx]
+            answer = 1
+            indices = set()
+            for i in range(idx):
+                it = arr[i]
+                if n % it:
+                    continue
+                j = bisect_left(arr, n // it, hi=idx)
+                if not (0 <= j < idx) or arr[j] != n // it:
+                    continue
+                
+                indices.add( (i, j) )
+            
+            for i, j in indices:
+                answer += (fn(i) * fn(j))
+
+            return answer % (10 ** 9 + 7)
+        
+        return sum(fn(i) for i in range(len(arr))) % (10 ** 9 + 7)
+                
+            
