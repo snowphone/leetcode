feb23837d3ee6fad723e29711d1c9e087b36a4f1
Junoh Moon <mjo970625@gmail.com>
2023-11-07 13:38:08 +0900

    Time: 1188 ms (5.31%), Space: 19.3 MB (12.62%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,33 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        """
+        1. Acyclic
+        2. Visit all nodes
+        """
+        graph = defaultdict(list)
+
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+
+        def dfs(prev, nd, visited): 
+            for adj in graph[nd]:
+                if adj in [nd, prev]:
+                    continue
+                if adj in visited:
+                    return None
+                
+                visited.add(adj)
+                if dfs(nd, adj, visited) is None:
+                    return None
+            return visited
+        
+        for i in range(n):
+            tmp = dfs(None, i, {i})
+            if tmp is None:
+                continue
+            if len(tmp) == n:
+                return True
+            return False
+            
