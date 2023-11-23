1bbb3a40b4ada62c96b0f8184a1d588473c9fbd5
Junoh Moon <mjo970625@gmail.com>
2023-11-23 10:08:25 +0900

    Time: 198 ms (25.84%), Space: 16.4 MB (69.71%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -2,10 +2,15 @@ class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         answer = []
         for fst, lst in zip(l, r):
-            tmp = sorted( nums[fst:lst+1] )
-            target = tmp[1] - tmp[0]
-
+            bag = set(nums[fst:lst+1])
+            m, M = min(nums[fst:lst+1]), max(nums[fst:lst+1])
+            diff = (M - m) / (lst - fst)
+            
             answer.append(
-                all( tmp[i] - tmp[i-1] == target for i in range(1, lst - fst + 1) )
+                all(
+                    m + i * diff in bag
+                    for i in range(lst - fst + 1)
+                )
             )
+                
         return answer
