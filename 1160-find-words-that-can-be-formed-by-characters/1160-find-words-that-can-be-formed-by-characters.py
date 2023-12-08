eefe08e7ca9006178a81ed3b8ab71cedd460b56d
Junoh Moon <mjo970625@gmail.com>
2023-12-08 14:49:32 +0900

    Time: 174 ms (35.77%), Space: 16.9 MB (36.2%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,20 +1,19 @@
 from collections import Counter
 
-def subset(big, small):
-    bigcnt = Counter(big)
-    smallcnt = Counter(small)
-
-    for ch in smallcnt.keys():
-        if smallcnt[ch] > bigcnt.get(ch, 0):
-            return False
-    return True
-
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        bigcnt = Counter(chars)
+        def subset(small):
+            smallcnt = Counter(small)
+        
+            for ch in smallcnt.keys():
+                if smallcnt[ch] > bigcnt.get(ch, 0):
+                    return False
+            return True
         answer = 0
 
         for it in words:
-            if subset(chars, it):
+            if subset(it):
                 answer += len(it)
 
         return answer
