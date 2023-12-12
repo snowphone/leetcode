404f48d8c0b127d4e2fbdd6bd815777eabc3656b
Junoh Moon <mjo970625@gmail.com>
2023-12-12 21:18:13 +0900

    Time: 442 ms (79.22%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
 
-
 select 
-  t.x,
-  t.y,
-  t.z,
-  case when greatest(t.x, t.y, t.z) * 2 < t.x + t.y + t.z then 'Yes' else 'No' end as triangle
-from triangle t;
+    t.x,
+    t.y,
+    t.z,
+    case
+        when greatest(t.x, t.y, t.z) * 2 < t.x + t.y + t.z then 'Yes'
+        else 'No'
+    end as triangle
+from triangle t
