13ecddb7e79d8475beb3332240b62b9bd4037e24
Junoh Moon <mjo970625@gmail.com>
2023-12-12 23:03:40 +0900

    Time: 1077 ms (23.16%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,17 +1,20 @@
 # Write your MySQL query statement below
 
-with cte as (
-    select p.product_id, max(p.change_date) as change_date
-    from Products p
-    where p.change_date <= '2019-08-16'
-    group by p.product_id
+select p.product_id, 10 as price
+from Products p
+where p.product_id not in (
+    select q.product_id
+    from Products q
+    where q.change_date <= '2019-08-16'
 )
-(
-    select c.product_id, p.new_price as price
-    from cte c
-    join Products p on c.product_id = p.product_id and c.change_date = p.change_date
-) union (
-    select distinct p.product_id, 10 as price
-    from Products p
-    where p.product_id not in (select p.product_id from cte p)
+
+union
+
+select p.product_id, p.new_price as price
+from Products p
+where (p.product_id, p.change_date) in (
+    select q.product_id, max(q.change_date) as change_date
+    from Products q
+    where q.change_date <= '2019-08-16'
+    group by q.product_id
 )
