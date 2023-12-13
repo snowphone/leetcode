313edcbceca54471a2b35d7f8ff9f84ddcb220cb
Junoh Moon <mjo970625@gmail.com>
2023-12-14 05:35:04 +0900

    Time: 641 ms (65.7%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+with dates as (
+    select distinct visited_on as date
+    from Customer
+    order by visited_on
+)
+select
+    d.date as visited_on,
+    sum(c.amount) as amount,
+    round( sum(c.amount) / count(distinct c.visited_on), 2) as average_amount
+from dates d
+join Customer c on c.visited_on between d.date - interval 6 day and d.date
+group by d.date
+having count(distinct c.visited_on) = 7
+order by d.date
