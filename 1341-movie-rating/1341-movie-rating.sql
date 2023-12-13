36314ee1db59deb1f309551daa024ea2f9a5db1d
Junoh Moon <mjo970625@gmail.com>
2023-12-13 23:29:14 +0900

    Time: 2466 ms (36.55%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -2,19 +2,18 @@
 
 (
     select u.name as results
-    from MovieRating mr
-    join Users u on u.user_id = mr.user_id
+    from Users u
+    join MovieRating mr on mr.user_id = u.user_id
     group by u.user_id, u.name
-    order by count(mr.rating) desc, u.name asc
+    order by count(*) desc, u.name asc
     limit 1
 )
 union all
 (
     select m.title as results
-    from MovieRating mr
-    join Movies m on m.movie_id = mr.movie_id
-    where mr.created_at between '2020-02-01' and '2020-02-29'
-    group by mr.movie_id
+    from Movies m
+    join MovieRating mr on mr.created_at between '2020-02-01' and '2020-02-29' and m.movie_id = mr.movie_id
+    group by m.movie_id, m.title
     order by avg(mr.rating) desc, m.title asc
     limit 1
 )
