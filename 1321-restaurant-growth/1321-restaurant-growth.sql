e2244f5ae73425d8671ae7ec27647f7beaadb67a
Junoh Moon <mjo970625@gmail.com>
2023-12-14 05:43:29 +0900

    Time: 617 ms (76.75%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -8,7 +8,7 @@ with dates as (
 select
     d.date as visited_on,
     sum(c.amount) as amount,
-    round( sum(c.amount) / count(distinct c.visited_on), 2) as average_amount
+    round( sum(c.amount) / 7., 2) as average_amount
 from dates d
 join Customer c on c.visited_on between d.date - interval 6 day and d.date
 group by d.date
