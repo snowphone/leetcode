e7874a213e853a674f9f7b06a1a5a937e3e54ca1
Junoh Moon <mjo970625@gmail.com>
2023-12-14 13:22:53 +0900

    Time: 855 ms (76.3%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,32 +1,13 @@
 # Write your MySQL query statement below
 
-with uniques as (
-    select 
-        min(i.pid) as pid,
-        min(i.lat) as lat,
-        min(i.lon) as lon
-    from Insurance i
-    group by i.tiv_2015
-    having count(*) = 1
-),
-joined as (
-    select i.pid
-    from Insurance i
-    join uniques u
-        on i.pid != u.pid
-        and i.lat = u.lat
-        and i.lon = u.lon
-)
 select round(sum(i.tiv_2016), 2) as tiv_2016
 from Insurance i
-where i.pid not in (
-    select pid from uniques
-    union all
-    select pid from joined
-) and not exists (
-    select *
+where (i.lat, i.lon) not in (
+    select j.lat, j.lon
     from Insurance j
     where i.pid != j.pid
-    and i.lat = j.lat
-    and i.lon = j.lon
+) and i.tiv_2015 in (
+    select j.tiv_2015
+    from Insurance j
+    where i.pid != j.pid 
 )
