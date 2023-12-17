cf2f3f9cd7c465a8398074e7a5ef79b6455d660e
Junoh Moon <mjo970625@gmail.com>
2023-12-17 13:16:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -14,26 +14,23 @@ class FoodRatings:
         self.q = defaultdict(
             lambda: SortedList(key=lambda it: (-it[0], it))
         )  # cuisine -> (rating, food) queue
+
         for food, cuisine, rating in zip(foods, cuisines, ratings):
             self.q[cuisine].add((rating, food))
-            self.lookup[food] = (cuisine, (rating, food))
+            key = self.q[cuisine].index((rating, food))
+            self.lookup[food] = (cuisine, key)
         return
 
     def changeRating(self, food: str, new_rating: int) -> None:
         cuisine, old_key = self.lookup[food]
-        self.q[cuisine].remove(old_key)
+        del self.q[cuisine][old_key]
 
-        new_key = (new_rating, food)
-        self.q[cuisine].add(new_key)
+        new_entry = (new_rating, food)
+        self.q[cuisine].add(new_entry)
 
+        new_key = self.q[cuisine].index(new_entry)
         self.lookup[food] = (cuisine, new_key)
         return
 
     def highestRated(self, cuisine: str) -> str:
         return self.q[cuisine][0][1]
-
-
-# Your FoodRatings object will be instantiated and called as such:
-# obj = FoodRatings(foods, cuisines, ratings)
-# obj.changeRating(food,newRating)
-# param_2 = obj.highestRated(cuisine)
