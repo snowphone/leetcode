43d9207b7957d5b9e26b6227d79f4d928b79369f
Junoh Moon <mjo970625@gmail.com>
2023-12-24 10:06:00 +0900

    Time: 39 ms (97.27%), Space: 17.3 MB (5.67%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,6 +1,7 @@
+from itertools import cycle
+
 class Solution:
     def minOperations(self, s: str) -> int:
-        n = len(s)
         def diff(a, b):
             return sum(
                 it != jt
@@ -8,6 +9,6 @@ class Solution:
             )
         
         return min(
-            diff(s, '10' * n),
-            diff(s, '01' * n),
+            diff(s, cycle("01")),
+            diff(s, cycle("10")),
         )
