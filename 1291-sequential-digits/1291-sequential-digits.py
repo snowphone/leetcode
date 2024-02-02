84849060af7fc99631bb1937b47dead0450282c2
Junoh Moon <mjo970625@gmail.com>
2024-02-02 09:22:22 +0900

    Time: 34 ms (71.12%), Space: 16.4 MB (72.98%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -3,13 +3,8 @@ class Solution:
         template = "123456789"
         low_len = len(str(low))
         high_len = len(str(high))
-        answer = []
-        for digit in range(low_len, high_len + 1):
-            for i in range(10 - digit):
-                numstr = template[i:i+digit]
-                n = int(numstr)
-                if not (low <= n <= high):
-                    continue
-                answer.append(n)
-            
-        return answer
+        return [ it 
+            for digit in range(low_len, high_len + 1)
+            for i in range(10 - digit)
+            if low <= (it := int( template[i:i+digit] ) ) <= high
+        ]
