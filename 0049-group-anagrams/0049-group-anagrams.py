1ce0dfcb50dbb363d7b37bc895eba6da013beb87
Junoh Moon <mjo970625@gmail.com>
2024-02-06 09:10:46 +0900

    Time: 86 ms (78.8%), Space: 20.2 MB (69.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,12 @@
+from collections import Counter
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        counter = defaultdict(list)
-        for s in strs:
-            counter[''.join(sorted(s))].append(s)
-        return list(counter.values())
+        @cache
+        def key(s):
+            return sorted(s)
+        
+        return [
+            list(group)
+            for k, group in groupby(sorted(strs, key=key), key=key)
+        ]
