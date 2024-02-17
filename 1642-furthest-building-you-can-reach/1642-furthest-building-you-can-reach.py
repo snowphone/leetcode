df957733fbe46dfcef1860258a0ab87d26323b20
Junoh Moon <mjo970625@gmail.com>
2024-02-17 21:12:18 +0900

    Time: 424 ms (61.61%), Space: 31 MB (73.99%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+class PQ(list):
+    def put(self, item):
+        heappush(self, item)
+
+    def get(self):
+        return heappop(self)
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        n = len(heights)
+        q = PQ()
+
+        for i in range(n - 1):
+            height = heights[i+1] - heights[i]
+            if height <= 0:
+                continue
+            q.put(height)
+            if len(q) > ladders:
+                bricks -= q.get()
+            if bricks < 0:
+                return i
+                
+        return n - 1
