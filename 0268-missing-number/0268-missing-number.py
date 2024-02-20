4288bc056eedf49cd192e1b3820226934d5196b4
Junoh Moon <mjo970625@gmail.com>
2024-02-20 09:36:53 +0900

    Time: 119 ms (38.96%), Space: 17.6 MB (98.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        sn = set(nums)
-        return next(
-            it for it in range(len(nums)+1) if it not in sn
-        )
+        n = len(nums)
+        acc = 0
+        for i in range(n + 1):
+            acc ^= i
+        for it in nums:
+            acc ^= it
+        return acc
