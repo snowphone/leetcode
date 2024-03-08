be34d9a61d0b6d5fd6ac208c8171c68631d73e0b
Junoh Moon <mjo970625@gmail.com>
2024-03-08 10:52:08 +0900

    Time: 85 ms (6.36%), Space: 55.6 MB (5.78%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.ts
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.ts
@@ -1,7 +1,12 @@
 function maxFrequencyElements(nums: number[]): number {
-	let counter = _(nums)
+	const counter = _(nums)
 		.countBy()
 		.commit()
-	const maxCnt = counter.entries().maxBy(([_, cnt]) => cnt)?.[1] ?? 0
-	return counter.entries().filter(([_, cnt]) => cnt == maxCnt).map(([_, cnt]) => cnt).sum()
+	const maxCnt = counter
+		.values()
+		.max()
+	return counter
+		.values()
+		.filter(it => it == maxCnt)
+		.sum()
 };
