e1d177810d3e8f93a5dc58beda16ffca139c2b2f
Junoh Moon <mjo970625@gmail.com>
2024-03-09 18:46:04 +0900

    Time: 80 ms (41.67%), Space: 69.1 MB (16.67%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.ts
+++ 2540-minimum-common-value/2540-minimum-common-value.ts
@@ -1,16 +1,16 @@
 function getCommon(nums1: number[], nums2: number[]): number {
-	return ((i: number, j: number) => {
+	function findFirst(i: number, j: number) {
 		if (i == nums1.length || j == nums2.length) {
 			return -1
 		}
 		const [lhs, rhs] = [nums1[i], nums2[j]]
 
 		if (lhs < rhs) {
-			return cmp(i + 1, j)
+			return findFirst(i + 1, j)
 		} else if (lhs > rhs) {
-			return cmp(i, j + 1)
+			return findFirst(i, j + 1)
 		}
 		return lhs
-	})(0, 0)
-
+	}
+	return findFirst(0, 0)
 };
