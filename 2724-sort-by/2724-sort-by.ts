21c09d2410f98df95c2ce6e51764ecf20ac20a67
Junoh Moon <mjo970625@gmail.com>
2024-03-09 23:44:35 +0900

    Time: 166 ms (6.08%), Space: 70.4 MB (5.76%) - LeetHub

---
+++ 2724-sort-by/2724-sort-by.ts
@@ -0,0 +1,8 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+	return _(arr)
+	.sortBy(fn)
+	.commit()
+};
