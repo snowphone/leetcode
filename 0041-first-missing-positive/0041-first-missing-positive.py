25e3be0734e7d28c3f9b52088b31fb2f116d36de
Junoh Moon <mjo970625@gmail.com>
2024-03-26 09:47:19 +0900

    Time: 277 ms (82.84%), Space: 35.2 MB (13.24%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,15 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        numset = { it for it in nums if it > 0 }
-        if not numset:
+        poss = [i for i in nums if i > 0]
+        if not poss:
             return 1
-        mini = min(numset)
-
-        if mini > 1:
+        m = min(poss)
+        M = max(poss)
+        if m > 1:
             return 1
-        
-        i = 1
-        while True:
-            if i not in numset:
-                return i
-            i += 1
+        posset = set(poss)
+        for i in range(m, M+2):
+            if i in posset:
+                continue
+            return i
