cdb8dfc29b8b63d4be95cf0d04bc174d4be2e113
Junoh Moon <mjo970625@gmail.com>
2024-07-17 12:53:51 +0900

    Time: 49 ms (87.07%), Space: 16.9 MB (93.78%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -5,43 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def _record_parents(self, root, parent):
-        parents = { root.val: parent } 
-        
+    def _traverse(self, root, parent):
+        answer = []
         if root.left:
-            parents |= self._record_parents(root.left, root)
+            answer += self._traverse(root.left, root)
         if root.right:
-            parents |= self._record_parents(root.right, root)
+            answer += self._traverse(root.right, root)
+        
+        if root.val in self.to_delete:
+            answer += [it for it in [root.left, root.right] if it]
 
-        return parents
-    
-    def _record_val_to_node(self, root):
-        mapping = {}
-        if root:
-            mapping[root.val] = root
-            mapping |= self._record_val_to_node(root.left)
-            mapping |= self._record_val_to_node(root.right)
-        return mapping
-
-    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        parents = self._record_parents(root, None)
-        mapping = self._record_val_to_node(root)
-
-        for to_del_val in to_delete:
-            nd = mapping[to_del_val]
-
-            parent = parents[to_del_val]
-            if parent and parent.left and parent.left.val == to_del_val:
+            if parent and parent.left and parent.left.val == root.val:
                 parent.left = None
-            if parent and parent.right and parent.right.val == to_del_val:
+            if parent and parent.right and parent.right.val == root.val:
                 parent.right = None
 
-            l = nd.left
-            r = nd.right
-            if l:
-                parents[l.val] = None
-            if r:
-                parents[r.val] = None
-        
-        existing_nodes = set(mapping.keys()) - set(to_delete)
-        return [ mapping[it] for it in existing_nodes if not parents[it] ]
+        return answer
+
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        self.to_delete = set(to_delete)
+
+        answer = self._traverse(root, None)
+        if root.val not in self.to_delete:
+            answer.append(root)
+        return answer
+
+
