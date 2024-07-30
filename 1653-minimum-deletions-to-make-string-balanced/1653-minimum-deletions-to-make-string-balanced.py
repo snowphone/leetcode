66f869ded532be67273146fa5f85ba34afc40be7
Junoh Moon <mjo970625@gmail.com>
2024-07-30 19:20:06 +0900

    Time: 878 ms (8.67%), Space: 17.6 MB (79%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,10 +1,11 @@
-from collections import Counter
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         n = len(s)
 
-        rhs = Counter(s)
-        lhs = {'a': 0, 'b': 0}
+        rhs = [0, 0]
+        for ch in s:
+            rhs[ord(ch) - ord('a')] += 1
+        lhs = [0, 0]
 
         answer = 987654321
 
@@ -14,10 +15,10 @@ class Solution:
         for i in range(n+1):
             answer = min(
                 answer,
-                lhs['b'] + rhs['a'],
+                lhs[1] + rhs[0],
             )
             if i < n:
                 ch = s[i]
-                lhs[ch] += 1
-                rhs[ch] -= 1
+                lhs[ord(ch) - ord('a')] += 1
+                rhs[ord(ch) - ord('a')] -= 1
         return answer
