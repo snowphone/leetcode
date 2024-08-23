572446745025d7d4d2af5be36c2ba87fe1bb8ede
Junoh Moon <mjo970625@gmail.com>
2024-08-23 13:36:15 +0900

    Time: 44 ms (12.02%), Space: 16.7 MB (26.36%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -1,10 +1,8 @@
 from math import gcd, lcm
+from operator import add, sub
 import re
 
 class Fraction:
-    numerator: int
-    denominator: int
-
     def __init__(self, numerator: int, denominator: int):
         self.numerator = numerator
         self.denominator = denominator
@@ -12,10 +10,6 @@ class Fraction:
     
     def __repr__(self):
         return f"{self.numerator}/{self.denominator}"
-
-    @staticmethod
-    def parse(s: str):
-        pass
 
     def __add__(self, rhs):
         lhs = self
@@ -28,63 +22,50 @@ class Fraction:
         return Fraction(numer, denom).reduced()
     
     def __sub__(self, rhs):
-        rhs.numerator *= -1
-        return self + rhs
+        return self + Fraction(
+            -rhs.numerator,
+            rhs.denominator,
+        )
     
     def reduced(self):
         common = gcd(self.numerator, self.denominator)
 
         return Fraction(self.numerator // common, self.denominator // common)
     
-def parse(s):
-    """
-    expr ::= num | binop
-    num ::= [0-9]+ | '-' [0-9]+
-    binop ::== frac [+-] frac
-    frac ::= num '/' num
-    """
-    tokenpattern = re.compile(r"\d+|[/+-]")
-    tokens = [it.group() for it in tokenpattern.finditer(s)]
-    stk = []
-    i = 0
-    # Fractionize
-    while i < len(tokens):
-        token = tokens[i]
-        if token in ['-', '+']:
-            if not stk:
-                stk.append(Fraction(0, 1))
-            stk.append(token)
-        elif token == '/':
-            stk.append('/')
-        else:
-            rhs = int(token)
-            if stk and stk[-1] == '/':
-               op = stk.pop()
-               lhs = stk.pop()
-               stk.append(Fraction(lhs, rhs)) 
-            else:
-                stk.append(rhs)
-        i += 1
-        
-    # Calc
-    stk, tokens = [], stk
-    i = 0
-    while i < len(tokens):
-        token = tokens[i]
-        if token == '-':
-            i += 1
-            next_token = tokens[i]
-            stk[-1] = stk[-1] - next_token
-        elif token == '+':
-            i += 1
-            next_token = tokens[i]
-            stk[-1] = stk[-1] + next_token
-        else:
-            stk.append(token)
-        i += 1
-
-    return stk[-1]
-
 class Solution:
     def fractionAddition(self, expression: str) -> str:
-        return str(parse(expression))
+        tokenpattern = re.compile(r"\d+|[/+-]")
+        tokens = [it.group() for it in tokenpattern.finditer(expression)]
+        stk = []
+        i = 0
+        # Fractionize
+        while i < len(tokens):
+            token = tokens[i]
+            if token in ['-', '+']:
+                if not stk:
+                    stk.append(Fraction(0, 1))
+                stk.append(token)
+            elif token == '/':
+                rhs = int(tokens[i+1])
+                i += 1
+                lhs = stk.pop()
+                stk.append(Fraction(lhs, rhs)) 
+            else:
+                stk.append(int(token))
+            i += 1
+            
+        # Add/sub
+        stk, tokens = [], stk
+        i = 0
+        op = { '-': sub, '+': add }
+        while i < len(tokens):
+            token = tokens[i]
+            if token in ['-', '+']:
+                i += 1
+                next_token = tokens[i]
+                stk[-1] = op[token](stk[-1] , next_token)
+            else:
+                stk.append(token)
+            i += 1
+    
+        return str(stk[-1])
