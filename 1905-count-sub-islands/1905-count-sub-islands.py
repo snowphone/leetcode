b7ea06778698a750fd9790eb21369b0537a90373
Junoh Moon <mjo970625@gmail.com>
2024-08-28 15:07:09 +0900

    Time: 2047 ms (68.7%), Space: 24.5 MB (93.72%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,65 +1,57 @@
 from queue import SimpleQueue
 
 ISLAND = 1
+VISITED = 2
 
 GridType = List[List[int]]
 
 class Solution:
     def countSubIslands(self, grid1: GridType, grid2: GridType) -> int:
-        islands = self._get_subislands(grid1, grid2)
-
-        return len(islands)
+        return self._get_subislands(grid1, grid2)
 
     def _get_subislands(self, main: GridType, sub: GridType):
         nrow=len(sub)
         ncol=len(sub[0])
 
-        visited = set()
-
-        islands = []
+        answer = 0
         for r in range(nrow):
             for c in range(ncol):
                 if not (sub[r][c] == ISLAND and main[r][c] == ISLAND):
                     continue
-                if (r, c) in visited:
-                    continue
-                island = self._scan(sub, main, r, c)
-                if island is None:
-                    continue
-                islands.append(island)
-                visited |= island
+                ok = self._scan(sub, main, r, c)
+                answer += int(ok)
 
-        return islands
+        return answer
     
-    def _scan(self, grid: GridType, main: GridType, start_r: int, start_c: int) -> frozenset[tuple[int, int]]|None:
+    def _scan(self, grid: GridType, main: GridType, start_r: int, start_c: int) -> bool:
         nrow=len(grid)
         ncol=len(grid[0])
 
-        bag = set()
+        answer = True
+
         q = SimpleQueue()
         q.put((start_r, start_c))
+        grid[start_r][start_c] = VISITED
 
-        def not_overlapped(r: int, c: int):
+        def only_on_main_island(r: int, c: int):
             return grid[r][c] == ISLAND and main[r][c] != ISLAND
 
         while not q.empty():
             r, c = q.get()
-            bag.add((r, c))
+
             for i, j in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
                 newr = r + i
                 newc = c + j
                 if (
                     not (0 <= newr < nrow) or
-                    not (0 <= newc < ncol)
+                    not (0 <= newc < ncol) or
+                    grid[newr][newc] != ISLAND 
                 ):
                     continue
-                if grid[newr][newc] != ISLAND:
-                    continue
-                if not_overlapped(newr, newc):
-                    return None
-                if (newr, newc) in bag:
-                    continue
+                if only_on_main_island(newr, newc):
+                    answer = False
                 q.put((newr, newc))
+                grid[newr][newc] = VISITED
 
-        return frozenset(bag)
+        return answer
         
