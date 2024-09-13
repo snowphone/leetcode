61712e4d8acc2a5e3693ef501907ceecb9f868ea
Junoh Moon <mjo970625@gmail.com>
2024-09-13 12:51:51 +0900

    Time: 310 ms (32.4%), Space: 31.1 MB (81.84%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,9 +1,20 @@
-from functools import reduce
-import operator
-
 class Solution:
+
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        return [
-            reduce(operator.xor, arr[l:r+1]) for l, r in queries
-        ]
+        self.prefix_sum = []
+        self._warm_up(arr)
+
+        return [ self._xor(l, r) for l, r in queries ]
+
+    def _warm_up(self, arr: List[int]): 
+        self.prefix_sum = arr[:1]
+        
+        for it in arr[1:]:
+            self.prefix_sum.append( it ^ self.prefix_sum[-1] )
+        return
+
+    def _xor(self, l: int, r: int):
+        if l == 0:
+            return self.prefix_sum[r]
+        return self.prefix_sum[r] ^ self.prefix_sum[l-1]
         
