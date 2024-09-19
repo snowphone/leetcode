7732ebacc8f05670b3c80599b357a60af1d2d9ae
Junoh Moon <mjo970625@gmail.com>
2024-09-19 14:06:22 +0900

    Time: 267 ms (5.06%), Space: 17.6 MB (8.08%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,30 +1,28 @@
 from dataclasses import dataclass
 import operator
 import re
-from typing import Protocol
+from typing import Protocol, TypeGuard
 
 
-class Intf(Protocol):
+class IValue(Protocol):
 
     def get(self) -> int:
         ...
 
 
 @dataclass(unsafe_hash=True)
-class Val(Intf):
-    n: int | Intf
+class Int(IValue):
+    n: int
 
     def get(self):
-        if isinstance(self.n, int):
-            return self.n
-        return self.n.get()
+        return self.n
 
 
 @dataclass(unsafe_hash=True)
 class Op:
     op: str
-    lhs: Intf
-    rhs: Intf
+    lhs: IValue
+    rhs: IValue
 
     def get(self):
         fn = {
@@ -39,26 +37,31 @@ class Solution:
 
     def diffWaysToCompute(self, expression: str) -> list[int]:
 
-        def mapper(x):
+        def mapper(x: str):
             if x not in '+-*':
-                return Val(n=int(x))
+                return Int(n=int(x))
             return x
 
         pattern = re.compile(r"[-+*]|\d+")
         tokens = [
             mapper(token.group()) for token in pattern.finditer(expression)
         ]
-        return [it.get() for it in self._solve(tokens)]
+        return [it.get() for it in self._solve(tokens)]  # type: ignore
 
-    def _solve(self, tokens: list[Intf | str]) -> set[Intf]:
+    def _solve(self, tokens: list[IValue | str]) -> set[IValue]:
         n = len(tokens)
 
-        if n == 1:
-            return set(tokens)  # type: ignore
+        def only_value(tokens: list[IValue | str]) -> TypeGuard[list[IValue]]:
+            return len(tokens) == 1
+
+        if only_value(tokens):
+            return set(tokens)
 
         answer = set()
         for i in range(0, n - 2, 2):
-            lhs, op, rhs = tokens[i:i + 3]
+            lhs: IValue = tokens[i]  # type: ignore
+            op: str = tokens[i + 1]  # type: ignore
+            rhs: IValue = tokens[i + 2]  # type: ignore
 
             answer |= self._solve([
                 *tokens[:i],
