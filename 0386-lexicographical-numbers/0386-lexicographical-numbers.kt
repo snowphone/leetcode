60eac9a502af70cae04750ccb1ccc91014daa987
Junoh Moon <mjo970625@gmail.com>
2024-09-21 21:29:35 +0900

    Time: 307 ms (31.82%), Space: 41.9 MB (59.09%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun lexicalOrder(n: Int): List<Int> {
+        val answer = mutableListOf<Int>()
+
+        for (i in 1..9) {
+            if (i <= n) {
+                answer.add(i)
+                this.generate(i.toString(), n, answer)
+            }
+        }
+        return answer
+    }
+
+    private fun generate(prefix: String, limit: Int, answer: MutableList<Int>) {
+        for (i in 0..9) {
+            val candidate = "${prefix}${i}"
+            val candidate_num = candidate.toInt()
+
+            if (candidate_num <= limit) {
+                answer.add(candidate_num)
+                this.generate(candidate, limit, answer)
+            }
+        }
+    }
+}
