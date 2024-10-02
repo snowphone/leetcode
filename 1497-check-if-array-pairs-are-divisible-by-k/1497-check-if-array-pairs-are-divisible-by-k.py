005b2b91437e7fac993294b56c1e74457231f433
Junoh Moon <mjo970625@gmail.com>
2024-10-02 09:59:14 +0900

    Time: 1294 ms (5.13%), Space: 31.3 MB (15.85%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        indexer = lambda x: (k + x % k) % k
+        counter = Counter(map(indexer, arr))
+
+        def pop(it):
+            counter[it] -= 1
+            if not counter[it]:
+                del counter[it]
+            return
+
+        while counter:
+            it = next(iter(counter.keys()))
+            pop(it)
+            target  = indexer(k - it)
+            print(it, target)
+            if target not in counter:
+                return False
+            pop(target)
+
+        return True
+        
+
+
