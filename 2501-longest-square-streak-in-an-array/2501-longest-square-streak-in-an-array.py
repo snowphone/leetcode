629b50849f2b1b04d9c1585d2e9c8e10da219ea0
Junoh Moon <mjo970625@gmail.com>
2024-10-28 18:07:28 +0900

    Time: 143 ms (40.54%), Space: 65.6 MB (5.33%) - LeetHub

---
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        nums.sort()
+        numset = set(nums)
+        sz = len(nums)
+
+        @cache
+        def solve(num):
+            if num * num in numset:
+                return 1 + solve(num*num)
+            else:
+                return 0
+        
+        answer = max( solve(it) for it in nums )
+        if answer == 0:
+            return -1
+        return answer + 1
+        
