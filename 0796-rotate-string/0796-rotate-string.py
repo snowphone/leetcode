fce6c92c4cd5886298ecadee6e3e64f8b42d793c
Junoh Moon <mjo970625@gmail.com>
2024-11-03 20:05:53 +0900

    Time: 0 ms (100%), Space: 16.7 MB (9.22%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -3,8 +3,8 @@ class Solution:
         n = len(s)
 
         return any(
-            self._shift(s, i) == goal for i in range(n)
+            self._shift(s, i) == goal for i in range(n+1)
         )
     
     def _shift(self, s: str, n: int):
-        return s[2:] + s[:2]
+        return s[n:] + s[:n]
