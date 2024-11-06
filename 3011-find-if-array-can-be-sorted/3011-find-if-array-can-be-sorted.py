34b8461d6dea39e1b0e6770d0b7af4d017d37a1c
Junoh Moon <mjo970625@gmail.com>
2024-11-06 15:53:43 +0900

    Time: 11 ms (57.27%), Space: 16.6 MB (43.17%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.py
@@ -1,15 +1,28 @@
+
+class Minmax:
+    def __init__(self, min: int, max: int):
+        self.min = min
+        self.max = max
+        return
+    
+    def __le__(self, other: Self):
+        return self.max <= other.min
+    
+    def update(self, num: int):
+        self.min = min(self.min, num)
+        self.max = max(self.max, num)
+
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        bits = [ [nums[0], nums[0]] ]  # (min, max)
+        bits = [ Minmax(nums[0], nums[0]) ]
         for it in nums[1:]:
             bc = int.bit_count(it)
-            if bc == int.bit_count(bits[-1][0]):
-                bits[-1][0] = min(bits[-1][0], it)
-                bits[-1][1] = max(bits[-1][1], it)
+            if bc == int.bit_count(bits[-1].min):
+                bits[-1].update(it)
             else:
-                bits.append( [it, it] )
+                bits.append( Minmax(it, it) )
         
         return all(
-            it[1] <= jt[0] for it, jt in zip(bits, bits[1:])
+            it <= jt for it, jt in zip(bits, bits[1:])
         )
     
