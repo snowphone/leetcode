2d702f0d4503a4a7b795c4b33d029c75677818b2
Junoh Moon <mjo970625@gmail.com>
2024-12-04 12:49:23 +0900

    Time: 121 ms (13.2%), Space: 17.8 MB (13.74%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -7,12 +7,14 @@ class Solution:
             rit = str2[r]
             if self._sim(lit, rit):
                 r += 1
+
         return r == len(str2)
     
-    def _dec(self, s: str):
-        chars = [chr(ord(it) - 1) if it != 'a' else 'z' for it in s]
-        return ''.join(chars)
+    def _inc(self, ch: str):
+        if ch == 'z':
+            return 'a'
+        return chr(ord(ch) + 1)
 
     def _sim(self, lhs, rhs):
-        return lhs in [rhs, self._dec(rhs)]
+        return rhs in [lhs, self._inc(lhs)]
         
