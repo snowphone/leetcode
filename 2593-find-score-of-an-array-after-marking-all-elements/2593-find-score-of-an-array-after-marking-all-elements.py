92464e160ca23f50193e4712941968ed02339770
Junoh Moon <mjo970625@gmail.com>
2024-12-13 14:12:09 +0900

    Time: 742 ms (14.55%), Space: 51.5 MB (5.47%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,21 +1,21 @@
 from heapq import heappush, heappop
 
 class HQ:
-    def __init__(self, fn):
+    def __init__(self):
         self.q = []
-        self.fn = fn
         return
     
     def put(self, item):
-        heappush(self.q, (self.fn(item), item))
+        heappush(self.q, item)
+        return
 
     def get(self):
-        return heappop(self.q)[1]
+        return heappop(self.q)
 
 class Solution:
     def findScore(self, nums: List[int]) -> int:
         marked = [False for _ in nums]
-        q = HQ(lambda it: it)
+        q = HQ()
         for i, it in enumerate(nums):
             q.put((it, i))
         remaining = set(range(len(nums)))
