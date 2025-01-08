c487da270a0a089485462700c24c9e1335dd0fc1
Junoh Moon <mjo970625@gmail.com>
2025-01-08 14:43:34 +0900

    Time: 27 ms (6.63%), Space: 17.7 MB (23.73%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.py
@@ -1,10 +1,45 @@
+class Trie:
+    def __init__(self):
+        self.terminal = object()
+        self.root = {}
+    
+    def add(self, word: str):
+        nd = self.root
+        for ch in word:
+            nd = nd.setdefault(ch, {})
+        nd[self.terminal] = True
+    
+    def starts_with(self, word: str):
+        nd = self.root
+        for ch in word:
+            if ch not in nd:
+                return False
+            nd = nd[ch]
+        return True
+
+
+class SuffixTrie(Trie):
+    def add(self, word: str):
+        super().add(word[::-1])
+    
+    def starts_with(self, word: str):
+        return super().starts_with(word[::-1])
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         n = len(words)
         answer = 0
-        for i, fix in enumerate(words):
-            for j in range(i+1, n):
-                word = words[j]
-                if word.startswith(fix) and word.endswith(fix):
+
+        for j in range(n):
+            prefix_trie = Trie()
+            suffix_trie = SuffixTrie()
+            word = words[j]
+            prefix_trie.add(word)
+            suffix_trie.add(word)
+
+            for i in range(j):
+                fix = words[i]
+                if prefix_trie.starts_with(fix) and suffix_trie.starts_with(fix):
                     answer += 1
+
         return answer
