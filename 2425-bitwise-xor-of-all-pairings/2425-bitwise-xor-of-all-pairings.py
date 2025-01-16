4df0a84acb5bd6b8730cf3d0b091ccbe515c6e93
Junoh Moon <mjo970625@gmail.com>
2025-01-16 11:23:18 +0900

    Time: 0 ms (100%), Space: 36.6 MB (27.17%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -3,10 +3,11 @@ from functools import reduce
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
         '''
-        if num2 even -> ignore nums1
-        if 
+        nums1 = [a, b]
+        nums2 = [1,2,3]
+        cartesian xor = [a1, a2, a3, b1, b2, b3]
+        answer = aaa bbb 11 22 33
         '''
-
         sum = lambda iter: reduce(operator.xor, iter)
 
         if len(nums2) & 1:
@@ -18,6 +19,6 @@ class Solution:
             rhs = sum(nums2)
         else:
             rhs = 0
-            
+
         return lhs ^ rhs
         
