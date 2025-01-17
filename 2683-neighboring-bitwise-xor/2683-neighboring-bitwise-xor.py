a0f3e1e2217291bcaa286566d429bb68d68192da
Junoh Moon <mjo970625@gmail.com>
2025-01-17 10:37:15 +0900

    Time: 43 ms (67.15%), Space: 22.4 MB (24.82%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,15 +1,24 @@
+from functools import reduce
+import operator
+
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        n = len(derived)
-        original = [0 for _ in range(n)]
+        """
+        d[i] = x[i] ^ x[i+1]
+        x[i+1] = d[i] ^ x[i]
+        x[i+1] = d[i] ^ (d[i-1] ^ x[i-1])
+        ...
+        x[i+1] = d[i] ^ (d[i-1] ^ x[i-1]) ^ ...^ d[0] ^ x[0]
 
-    
+        Therefore, the `x` must satisfy x[n] = reduce(xor, d[0..n-1]) ^ x[0]
+        """
+        xor = lambda iter: reduce(operator.xor, iter)
+        n = len(derived)
+
+        acc = xor(derived)
+
         def try_with(start_value: int):
-            original[0] = start_value
-            for i, d in enumerate(derived):
-                original[(i + 1) % n] = d ^ original[i]
-            
-            return original[0] == start_value
+            return start_value == (acc ^ start_value)
         
         return try_with(0) or try_with(1)
         
