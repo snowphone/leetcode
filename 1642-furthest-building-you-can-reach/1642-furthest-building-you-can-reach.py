c8c610c19d9f8fce583d2faac2090779d9932bd9
Junoh Moon <mjo970625@gmail.com>
2025-01-21 05:02:51 +0900

    Time: 210 ms (9.33%), Space: 29.1 MB (12.63%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,24 +1,35 @@
-from heapq import heappush, heappop
-class PQ(list):
+from sortedcontainers import SortedList
+
+
+class PQ:
+    "Sorted in ascending order"
+    def __init__(self):
+        self.q = SortedList()
+
     def put(self, item):
-        heappush(self, item)
+        return self.q.add(item)
 
     def get(self):
-        return heappop(self)
+        return self.q.pop(0)
+
+    def __bool__(self):
+        return bool(self.q)
+
 
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        n = len(heights)
-        q = PQ()
-
-        for i in range(n - 1):
-            height = heights[i+1] - heights[i]
-            if height <= 0:
+        delta_q = PQ()
+        for idx in count(0):
+            if idx == len(heights) - 1:
+                return idx
+            current, nxt = heights[idx : idx + 2]
+            if current >= nxt:
                 continue
-            q.put(height)
-            if len(q) > ladders:
-                bricks -= q.get()
-            if bricks < 0:
-                return i
-                
-        return n - 1
+            delta_q.put(nxt - current)
+            if ladders:
+                ladders -= 1
+            elif delta_q and (delta := delta_q.get()) <= bricks:
+                bricks -= delta  # Replace a ladder with bricks
+            else:
+                break
+        return idx
