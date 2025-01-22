7cfadc13dbe7fa9887caa7c551fdb1add33988f1
Junoh Moon <mjo970625@gmail.com>
2025-01-22 11:06:48 +0900

    Time: 1059 ms (51.29%), Space: 86.2 MB (38.12%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -15,39 +15,31 @@ class Solution:
 
         q = SimpleQueue[Entry]()
 
-        def put(item: Entry):
-            q.put(item)
-            return
-
-        def get():
-            return q.get()
-
         for r in range(nrow):
             for c in range(ncol):
                 if isWater[r][c]:
                     matrix[r][c] = 0
-                    put((r, c, 0))
+                    q.put((r, c, 0))
 
         def get_adjacents(r: int, c: int):
             for i, j in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
                 adj_r = r + i
                 adj_c = c + j
-                if not (0 <= adj_r < nrow):
-                    continue
-                if not (0 <= adj_c < ncol):
-                    continue
-                if isWater[adj_r][adj_c]:
-                    continue
-                if matrix[adj_r][adj_c] != UNUSED:
+                if (
+                    not (0 <= adj_r < nrow) or
+                    not (0 <= adj_c < ncol) or
+                    isWater[adj_r][adj_c] or
+                    matrix[adj_r][adj_c] != UNUSED
+                ):
                     continue
                 yield (adj_r, adj_c)
 
         while not q.empty():
-            r, c, h = get()
+            r, c, h = q.get()
 
             for adj_r, adj_c in get_adjacents(r, c):
                 new_h = h + 1
                 matrix[adj_r][adj_c] = new_h
-                put((adj_r, adj_c, new_h))
+                q.put((adj_r, adj_c, new_h))
 
         return matrix
