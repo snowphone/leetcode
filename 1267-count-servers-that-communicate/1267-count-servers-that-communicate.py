d5e2e8842d98bd758405b42c558e5c57927dd6fe
Junoh Moon <mjo970625@gmail.com>
2025-01-25 10:15:41 +0900

    Time: 47 ms (8.83%), Space: 20.2 MB (8.02%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -4,27 +4,31 @@ class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         adjacents = self._get_graph(grid)
             
-        return sum(
-            1 for node in adjacents.keys() if adjacents[node]
-        )
+        return sum( adj for node, adj in adjacents.items() )
     
     def _get_graph(self, grid: list[list[int]]):
-        adjacents = dict()
         nrow = len(grid)
         ncol = len(grid[0])
+        nodes = {
+            (r, c)
+            for r in range(nrow)
+            for c in range(ncol)
+            if grid[r][c]
+        }
+        
         rows = defaultdict(set)
         cols = defaultdict(set)
-        nodes = set()
-        for r in range(nrow):
-            for c in range(ncol):
-                if not grid[r][c]:
-                    continue
-                node = (r, c)
-                nodes.add( node )
-                rows[r].add( node )
-                cols[c].add( node )
-
         for node in nodes:
             r, c = node
-            adjacents[node] = (rows[r] | cols[c]) - {node}
+            rows[r].add( node )
+            cols[c].add( node )
+
+        adjacents = {nd: False for nd in nodes}
+        for node in nodes:
+            r, c = node
+            for adj in chain(rows[r], cols[c]):
+                if adj == node:
+                    continue
+                adjacents[adj] = True
+
         return adjacents
