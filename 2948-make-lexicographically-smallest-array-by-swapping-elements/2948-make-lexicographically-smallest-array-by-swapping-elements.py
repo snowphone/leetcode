71918b46bf064cb440b772d7aef2ce2a8601ce2e
Junoh Moon <mjo970625@gmail.com>
2025-01-25 20:08:58 +0900

    Time: 258 ms (90.28%), Space: 96.5 MB (5.56%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,30 +1,29 @@
-from sortedcontainers import SortedList
 from collections import deque
 
+
 class Solution:
-    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        n = len(nums)
-        s = sorted(nums)
-        groups = [ deque(s[:1]) ]
-        for it in s[1:]:
-            if it - groups[-1][-1] <= limit:
-                groups[-1].append(it)
-            else:
-                groups.append( deque([it] ) )
-        
-        num_to_group = {
-            it: group
-            for group in groups
-            for it in group
-        }
+
+    def lexicographicallySmallestArray(self, nums: list[int], limit: int) -> List[int]:
+        groups = self._get_groups(nums, limit)
+        num_to_group = {it: group for group in groups for it in group}
 
         sz = len(nums)
         for i in range(0, sz):
             it = nums[i]
             group = num_to_group[it]
-
-            new_it = group.popleft()
-            new_it_idx = nums.index(new_it, i)
-            nums[i], nums[new_it_idx] = new_it, it
+            nums[i] = group.popleft()
 
         return nums
+
+    def _get_groups(self, nums: list[int], limit: int):
+        n = len(nums)
+        s = sorted(nums)
+        groups = [deque(s[:1])]
+        sz = len(nums)
+        for i in range(1, sz):
+            it = s[i]
+            if it - groups[-1][-1] <= limit:
+                groups[-1].append(it)
+            else:
+                groups.append(deque([it]))
+        return groups
