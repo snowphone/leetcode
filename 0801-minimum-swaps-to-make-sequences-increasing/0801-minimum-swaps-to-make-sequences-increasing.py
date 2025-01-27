7d7b28d0aadcfa3db725e58792ebb0edf5e12527
Junoh Moon <mjo970625@gmail.com>
2025-01-27 22:37:50 +0900

    Time: 805 ms (5.13%), Space: 134.4 MB (11.86%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+
+        def get(idx: int, swapped: Literal[0, 1]):
+            if swapped:
+                return nums2[idx], nums1[idx]
+            return nums1[idx], nums2[idx]
+
+        @cache
+        def solve(idx: int, swapped: Literal[0, 1]):
+            if idx == 0:
+                return swapped
+            
+            i, j = get(idx, swapped)
+
+            answer = 10 ** 5 + 7
+            for prev_swapped in [False, True]:
+                prev_i, prev_j = get(idx-1, prev_swapped)
+                if prev_i < i and prev_j < j:
+                    answer = min( answer, solve(idx-1, prev_swapped) + swapped )
+            return answer
+        
+        return min(solve(n-1, 0), solve(n-1, 1))
