74171ef9dc82b48682ffed06e1668d6c467628ad
Junoh Moon <mjo970625@gmail.com>
2025-02-08 09:27:51 +0900

    Time: 277 ms (42.65%), Space: 100.2 MB (21.57%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.py
@@ -0,0 +1,27 @@
+from sortedcontainers import SortedList
+
+class NumberContainers:
+
+    def __init__(self):
+        self.container = {}
+        self.rev = defaultdict(SortedList)
+
+    def change(self, index: int, number: int) -> None:
+        "O(n log(n))"
+        prev_number = self.container.get(index)
+        if prev_number is not None:
+            self.rev[prev_number].remove(index)
+
+        self.container[index] = number
+        self.rev[number].add(index)
+
+    def find(self, number: int) -> int:
+        'O(1)'
+        indices = self.rev[number]
+        return indices[0] if indices else -1
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
