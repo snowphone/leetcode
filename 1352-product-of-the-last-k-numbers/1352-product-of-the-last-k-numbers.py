598f2493a356f51bc1086e39a19e3f4f107addfb
Junoh Moon <mjo970625@gmail.com>
2025-02-14 09:23:41 +0900

    Time: 45 ms (36.53%), Space: 32.1 MB (13.55%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -1,23 +1,22 @@
-import operator
-from functools import reduce, partial
-
 class ProductOfNumbers:
-    prod = partial(reduce, operator.mul)
-
     def __init__(self):
         self.q = list()
         self.acc_q = [1]
+    
 
     def add(self, num: int) -> None:
-        self.q.append(num)
-        self.acc_q.append(num * self.acc_q[-1])
+        if num == 0:
+            self.__init__()
+        else:
+            self.q.append(num)
+            self.acc_q.append(num * self.acc_q[-1])
 
     def getProduct(self, k: int) -> int:
+        if len(self.q) < k:
+            return 0
         # a over b
         a = self.acc_q[-1]
         b = self.acc_q[-1-k]
-        if b == 0:
-            return self.prod(self.q[-k:])
         return a // b
 
 
