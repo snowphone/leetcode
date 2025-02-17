ceec5533ad738aa8b49dd6fff2bb8f9e407cea17
Junoh Moon <mjo970625@gmail.com>
2025-02-17 16:06:51 +0900

    Time: 200 ms (7.32%), Space: 19.7 MB (25.75%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -4,15 +4,15 @@ class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         return len(self.gen(tiles))
     
-    def gen(self, tiles: list[str]):
+    def gen(self, tiles: str):
         if len(tiles) == 1:
-            return { tuple(tiles) }
+            return { tiles }
         
         answer = set()
         for i, ch in enumerate(tiles):
-            popped = [it for j, it in enumerate(tiles) if j != i]
+            popped = ''.join([it for j, it in enumerate(tiles) if j != i])
             subanswer = self.gen(popped)
             answer |= subanswer
-            answer |= set( (ch, *jt) for jt in subanswer )
+            answer |= set( ch+jt for jt in subanswer )
 
         return answer
